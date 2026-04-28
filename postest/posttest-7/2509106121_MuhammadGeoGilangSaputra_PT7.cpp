5738c165e12d3902aa233050a07c8003ab47821d
geooo14 <geooo140607@gmail.com>
2026-04-28 22:29:30 +0800

    finishposttest

---
+++ postest/posttest-7/2509106121_MuhammadGeoGilangSaputra_PT7.cpp
@@ -0,0 +1,307 @@
+#include <iostream>
+#include <iomanip>
+#include <stdexcept> // library exception
+using namespace std;
+
+#define MAX_USER 10
+#define MAX_RENTAL 50
+
+//================= STRUCT =================
+struct Tanggal{
+    int hari, bulan, tahun;
+};
+
+struct Rental{
+    int id;
+    string namaPenyewa;
+    string playbox;
+    int lamaSewa;
+    int harga;
+    Tanggal tanggal;
+};
+
+struct User{
+    string nama;
+    string nim;
+};
+
+//================= DATA AWAL =================
+User user[MAX_USER] = {{"Geo","121"}};
+
+Rental dataRental[MAX_RENTAL] = {
+    {101,"Andi","PS5",2,50000,{1,1,2024}},
+    {102,"Budi","PS4",3,40000,{2,1,2024}},
+    {103,"Caca","PS3",1,30000,{3,1,2024}}
+};
+
+int jumlahUser = 1;
+int jumlahData = 3;
+
+//================= REGISTER =================
+void tambahUser(User *u, int *jumlah){
+    try{
+        if(*jumlah >= MAX_USER){
+            throw length_error("User penuh!");
+        }
+
+        cout<<"\n=== REGISTER ===\n";
+        cout<<"Nama : ";
+        cin>>u[*jumlah].nama;
+
+        cout<<"NIM  : ";
+        cin>>u[*jumlah].nim;
+
+        (*jumlah)++;
+    }
+    catch(const exception &e){
+        cout<<"Error: "<<e.what()<<endl;
+    }
+}
+
+//================= LOGIN =================
+bool login(User *u, int jumlah){
+    string nama,nim;
+
+    cout<<"\n=== LOGIN ===\n";
+    cout<<"Nama : "; cin>>nama;
+    cout<<"NIM  : "; cin>>nim;
+
+    for(int i=0;i<jumlah;i++){
+        if(nama==u[i].nama && nim==u[i].nim){
+            return true;
+        }
+    }
+    return false;
+}
+
+//================= TAMBAH DATA =================
+void tambahRental(Rental *r, int *jumlah){
+    try{
+        if(*jumlah >= MAX_RENTAL){
+            throw length_error("Data penuh!");
+        }
+
+        cout<<"\nID : ";
+        cin>>(r+*jumlah)->id;
+
+        if(cin.fail()){
+            throw invalid_argument("ID harus angka!");
+        }
+
+        if((r+*jumlah)->id < 0){
+            throw invalid_argument("ID tidak boleh negatif!");
+        }
+
+        cin.ignore();
+        cout<<"Nama Penyewa : ";
+        getline(cin,(r+*jumlah)->namaPenyewa);
+
+        cout<<"Playbox : ";
+        getline(cin,(r+*jumlah)->playbox);
+
+        cout<<"Lama Sewa : ";
+        cin>>(r+*jumlah)->lamaSewa;
+
+        cout<<"Harga : ";
+        cin>>(r+*jumlah)->harga;
+
+        if((r+*jumlah)->lamaSewa <= 0 || (r+*jumlah)->harga <= 0){
+            throw runtime_error("Lama sewa dan harga harus > 0!");
+        }
+
+        cout<<"Hari : ";
+        cin>>(r+*jumlah)->tanggal.hari;
+
+        cout<<"Bulan : ";
+        cin>>(r+*jumlah)->tanggal.bulan;
+
+        cout<<"Tahun : ";
+        cin>>(r+*jumlah)->tanggal.tahun;
+
+        int total = (r+*jumlah)->lamaSewa * (r+*jumlah)->harga;
+        cout<<"Total : "<<total<<endl;
+
+        (*jumlah)++;
+    }
+    catch(const exception &e){
+        cout<<"Error: "<<e.what()<<endl;
+        cin.clear();
+        cin.ignore(1000,'\n');
+    }
+}
+
+//================= TAMPIL =================
+void tampil(Rental *r, int jumlah){
+    cout<<"\n=== DATA ===\n";
+
+    for(int i=0;i<jumlah;i++){
+        cout<<i+1<<". "
+        <<"ID:"<< (r+i)->id<<" | "
+        <<(r+i)->namaPenyewa<<" | "
+        <<(r+i)->playbox<<" | "
+        <<(r+i)->lamaSewa<<" | "
+        <<(r+i)->harga<<" | "
+        <<(r+i)->tanggal.hari<<"/"
+        <<(r+i)->tanggal.bulan<<"/"
+        <<(r+i)->tanggal.tahun
+        <<endl;
+    }
+}
+
+//================= UPDATE =================
+void ubah(Rental *r, int jumlah){
+    try{
+        int idx;
+        tampil(r,jumlah);
+
+        cout<<"Pilih : ";
+        cin>>idx;
+
+        if(idx < 1 || idx > jumlah){
+            throw out_of_range("Index tidak valid!");
+        }
+
+        cin.ignore();
+        cout<<"Nama Baru : ";
+        getline(cin,(r+idx-1)->namaPenyewa);
+
+        cout<<"Playbox Baru : ";
+        getline(cin,(r+idx-1)->playbox);
+    }
+    catch(const exception &e){
+        cout<<"Error: "<<e.what()<<endl;
+    }
+}
+
+//================= DELETE =================
+void hapus(Rental *r, int *jumlah){
+    try{
+        int idx;
+        tampil(r,*jumlah);
+
+        cout<<"Pilih : ";
+        cin>>idx;
+
+        if(idx < 1 || idx > *jumlah){
+            throw out_of_range("Index tidak valid!");
+        }
+
+        for(int i=idx-1;i<*jumlah-1;i++){
+            *(r+i) = *(r+i+1);
+        }
+
+        (*jumlah)--;
+    }
+    catch(const exception &e){
+        cout<<"Error: "<<e.what()<<endl;
+    }
+}
+
+//================= SORT =================
+void sortID(Rental *r, int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if((r+j)->id > (r+j+1)->id){
+                swap(*(r+j), *(r+j+1));
+            }
+        }
+    }
+}
+
+//================= SEARCH =================
+int searchNama(Rental *r, int n, string cari){
+    for(int i=0;i<n;i++){
+        if((r+i)->namaPenyewa == cari){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int binarySearchID(Rental *r, int n, int target){
+    int low = 0, high = n-1;
+
+    while(low <= high){
+        int mid = (low + high) / 2;
+
+        if((r+mid)->id == target) return mid;
+        else if((r+mid)->id < target) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;
+}
+
+//================= MAIN =================
+int main(){
+    int pilih;
+
+    do{
+        cout<<"\n1.Register\n2.Login\n3.Keluar\nPilih : ";
+        cin>>pilih;
+
+        if(pilih==1){
+            tambahUser(user, &jumlahUser);
+        }
+
+        else if(pilih==2){
+            if(login(user,jumlahUser)){
+
+                int menu;
+                do{
+                    cout<<"\n1.Tambah\n2.Lihat\n3.Ubah\n4.Hapus\n5.Sorting ID\n6.Searching\n7.Logout\nPilih : ";
+                    cin>>menu;
+
+                    if(menu==1) tambahRental(dataRental,&jumlahData);
+                    else if(menu==2) tampil(dataRental,jumlahData);
+                    else if(menu==3) ubah(dataRental,jumlahData);
+                    else if(menu==4) hapus(dataRental,&jumlahData);
+
+                    else if(menu==5){
+                        sortID(dataRental, jumlahData);
+                        cout<<"Data berhasil diurutkan\n";
+                    }
+
+                    else if(menu==6){
+                        int pilihSearch;
+                        cout<<"\n1.Cari Nama\n2.Cari ID\nPilih : ";
+                        cin>>pilihSearch;
+
+                        if(pilihSearch==1){
+                            cin.ignore();
+                            string nama;
+                            cout<<"Masukkan nama : ";
+                            getline(cin,nama);
+
+                            int hasil = searchNama(dataRental, jumlahData, nama);
+
+                            if(hasil != -1)
+                                cout<<"Ditemukan: "<<(dataRental+hasil)->namaPenyewa<<endl;
+                            else
+                                cout<<"Tidak ditemukan\n";
+                        }
+
+                        else if(pilihSearch==2){
+                            int id;
+                            cout<<"Masukkan ID : ";
+                            cin>>id;
+
+                            int hasil = binarySearchID(dataRental, jumlahData, id);
+
+                            if(hasil != -1)
+                                cout<<"Ditemukan: "<<(dataRental+hasil)->namaPenyewa<<endl;
+                            else
+                                cout<<"Tidak ditemukan\n";
+                        }
+                    }
+
+                }while(menu!=7);
+            }
+            else{
+                cout<<"Login gagal!\n";
+            }
+        }
+
+    }while(pilih!=3);
+
+    return 0;
+}
